a3833fd4fe8e06d2729cea5d25a568f02ae32bdb
Faye Atendido <jasmine.f.g.atendido@accenture.com>
2023-08-02 17:58:24 +0800

    init'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parking-system';
+  entranceCount: number = 3;
+  entranceQueues = [0, 0, 0];
+  availableParkingSlots: Array<Array<number>> = [[4, 5, 6], [1, 2, 4], [6, 7 ,8], [9, 1, 4]];
+  parkingSlotSizes: Array<number> = [0, 2, 1, 1]
+
+  public parkVehicle(vehicle: Vehicle) {
+    const queueCount = this.entranceQueues.sort((a, b) => b - a)[0];
+    const entranceNumber = this.entranceQueues.findIndex(n => n === queueCount);
+    const freeSlot = this.availableParkingSlots.map(arr => arr[entranceNumber]).sort()[0];
+    // TODO: hindi na take into account yung size ni vehicle
+    vehicle.parkingSlot = this.availableParkingSlots.findIndex(arr => arr[entranceNumber] === freeSlot);
+    vehicle.timeIn = Date.now();
+  }
+
+  public unparkVehicle() {
+
+  }
+}
+
+class Vehicle {
+  parkingSlot: number = 0;
+  size: number = 0;
+  totalCharge: number = 0;
+  timeIn: number = 0;
+  timeOut: number = 0;
+
+  constructor(size: number) {
+    if (size > 3) throw new Error('Invalid vehicle size!');
+    this.size = size;
+  }
+}
+
+class ParkingSlot {
+  id: number = 0;
+  size: number = 0;
+  distances: Array<number> = [];
+
+  constructor(id: number, size: number, distances: Array<number>) {
+    this.id = id;
+    this.size = size;
+    this.distances = distances;
+  }
 }

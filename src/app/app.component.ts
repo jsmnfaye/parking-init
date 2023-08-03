75b7b4e0b0dfe17e13a56291dd401b9f937440bc
Faye Atendido <--local>
2023-08-03 21:59:01 +0800

    get slots according to vehicle size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent implements OnInit {
   title = 'parking-system';
   entranceCount: number = 3;
   entranceQueues = [0, 0, 0];
-  availableParkingSlots: Array<Array<number>> = [[4, 5, 6], [1, 2, 4], [6, 7 ,8], [9, 1, 4]];
+  availableParkingSlots: Array<Array<number>> = [[4, 5, 6], [1, 2, 4], [6, 7, 8], [9, 1, 4]];
   parkingSlotSizes: Array<number> = [0, 2, 1, 1]
   parkingSlots: Array<ParkingSlot> = [];
 
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
   mediumVehicle: Vehicle = new Vehicle(1);
   largeVehicle: Vehicle = new Vehicle(2);
 
-  ngOnInit() {    
+  ngOnInit() {
     if (this.availableParkingSlots.length !== this.parkingSlotSizes.length) {
       throw new Error('Different numbers of parking slot distances and sizes; should be equal!');
     }
@@ -30,19 +30,38 @@ export class AppComponent implements OnInit {
       );
     }
     console.log(this.parkingSlots);
+
+    this.parkVehicle(this.smallVehicle);
+    this.parkVehicle(this.mediumVehicle);
+    this.parkVehicle(this.largeVehicle);
   }
 
   public parkVehicle(vehicle: Vehicle) {
-    const queueCount = this.entranceQueues.sort((a, b) => b - a)[0];
-    const entranceNumber = this.entranceQueues.findIndex(n => n === queueCount);
-    const freeSlot = this.availableParkingSlots.map(arr => arr[entranceNumber]).sort()[0];
-    // TODO: hindi na take into account yung size ni vehicle
-    vehicle.parkingSlot = this.availableParkingSlots.findIndex(arr => arr[entranceNumber] === freeSlot);
+    const entranceNumber = this.getEntranceNumber();
+    // const freeSlot = this.availableParkingSlots.map(arr => arr[entranceNumber]).sort()[0];
+    const freeSlots = this.parkingSlots.filter(parkingSlot => 
+      // get slots that the car can fit in
+      (parkingSlot.isAvailable && vehicle.size === 2 && parkingSlot.size === 2) ||
+      (parkingSlot.isAvailable && vehicle.size === 1 && parkingSlot.size > 0) ||
+      (parkingSlot.isAvailable && vehicle.size === 0 && parkingSlot.size >= 0)
+    ).map(parkingSlot => parkingSlot.distances[entranceNumber]).sort()[0];
+    
+      // then sort the distances in ascending order with respect to entranceNumber
+
+    // TODO: set parking slot availability to 'false'
+    // TODO: set time in
+
+    // vehicle.parkingSlot = this.availableParkingSlots.findIndex(arr => arr[entranceNumber] === freeSlot);
     vehicle.timeIn = Date.now();
   }
 
   public unparkVehicle() {
 
+  }
+
+  private getEntranceNumber() {
+    const queueCount = this.entranceQueues.sort((a, b) => b - a)[0];
+    return this.entranceQueues.findIndex(n => n === queueCount);
   }
 }
 

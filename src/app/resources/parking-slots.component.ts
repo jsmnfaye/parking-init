8c1cd5ff1d4505250009b849e8ca021ac3050a5a
Faye Atendido <jsmnfaye@gmail.com>
2023-08-14 21:45:00 +0800

    standardized distances

--- src/app/resources/parking-slots.component.ts
+++ src/app/resources/parking-slots.component.ts
@@ -1,94 +1,94 @@
 export const PARKING_SLOTS = [
     {
-        "distances": [4, 5, 6],
+        "distances": [1, 7, 8],
         "size": 1
     },
     {
-        "distances": [1, 2, 4],
+        "distances": [2, 6, 7],
         "size": 2
     },
     {
-        "distances": [6, 7, 8],
+        "distances": [3, 5, 6],
         "size": 1
     },
     {
-        "distances": [9, 1, 4],
+        "distances": [4, 5, 5],
         "size": 0
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [5, 5, 4],
         "size": 0
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [6, 5, 1],
         "size": 2
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [8, 7, 1],
         "size": 1
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [8, 6, 2],
         "size": 1
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [1, 6, 8],
         "size": 0
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [2, 5, 7],
         "size": 1
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [6, 3, 3],
         "size": 0
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [7, 6, 2],
         "size": 2
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [8, 7, 3],
         "size": 1
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [7, 5, 10],
         "size": 1
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [8, 4, 9],
         "size": 0
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [7, 2, 6],
         "size": 0
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [8, 5, 7],
         "size": 0
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [9, 6, 8],
         "size": 0
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [8, 4, 11],
         "size": 1
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [7, 3, 10],
         "size": 1
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [8, 3, 8],
         "size": 2
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [9, 4, 9],
         "size": 2
     },
     {
-        "distances": [3, 2, 5],
+        "distances": [10, 5, 10],
         "size": 0
     }
 ]

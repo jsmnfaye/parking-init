e287b6bd205c8b8f580e33de82f98ca1c9b1308f
Faye Atendido <jsmnfaye@gmail.com>
2023-08-16 22:13:32 +0800

    added slot ids for clarity

--- src/app/resources/parking-slots.component.ts
+++ src/app/resources/parking-slots.component.ts
@@ -1,93 +1,116 @@
 export const PARKING_SLOTS = [
     {
+        "slot": 1,
         "distances": [1, 7, 8],
         "size": 1
     },
     {
+        "slot": 2,
         "distances": [2, 6, 7],
         "size": 2
     },
     {
+        "slot": 3,
         "distances": [3, 5, 6],
         "size": 1
     },
     {
+        "slot": 4,
         "distances": [4, 5, 5],
         "size": 0
     },
     {
+        "slot": 5,
         "distances": [5, 5, 4],
         "size": 0
     },
     {
+        "slot": 6,
         "distances": [6, 5, 1],
         "size": 2
     },
     {
+        "slot": 7,
         "distances": [8, 7, 1],
         "size": 1
     },
     {
+        "slot": 8,
         "distances": [8, 6, 2],
         "size": 1
     },
     {
+        "slot": 9,
         "distances": [1, 6, 8],
         "size": 0
     },
     {
+        "slot": 10,
         "distances": [2, 5, 7],
         "size": 1
     },
     {
+        "slot": 11,
         "distances": [6, 3, 3],
         "size": 0
     },
     {
+        "slot": 12,
         "distances": [7, 6, 2],
         "size": 2
     },
     {
+        "slot": 13,
         "distances": [8, 7, 3],
         "size": 1
     },
     {
+        "slot": 14,
         "distances": [7, 5, 10],
         "size": 1
     },
     {
+        "slot": 15,
         "distances": [8, 4, 9],
         "size": 0
     },
     {
+        "slot": 16,
         "distances": [8, 2, 6],
         "size": 0
     },
     {
+        "slot": 17,
         "distances": [8, 5, 7],
         "size": 0
     },
     {
+        "slot": 18,
         "distances": [9, 6, 8],
         "size": 0
     },
     {
+        "slot": 19,
         "distances": [8, 4, 11],
         "size": 1
     },
     {
+        "slot": 20,
         "distances": [7, 3, 10],
         "size": 1
     },
     {
+        "slot": 21,
         "distances": [8, 3, 8],
         "size": 2
     },
     {
+        "slot": 22,
         "distances": [9, 4, 9],
         "size": 2
     },
     {
+        "slot": 23,
         "distances": [10, 5, 10],
         "size": 0
     }
